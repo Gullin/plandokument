efdb8cb9a24707961f08bc96a1f72e584d780cf7
Christian Gullin <krich_80@hotmail.com>
2019-06-04 21:37:36 +0200

    Felstavning i SQL-sats plan berör plan tillrättad

--- src/class/PlanCache.cs
+++ src/class/PlanCache.cs
@@ -385,7 +385,7 @@ namespace Plan.Plandokument
         {
             string sql = string.Empty;
 
-            sql = "SELECT TO_CHAR(plan_id) AS nyckel, beskrivning AS beskrivning, TO_CHAR(pav_plan_id) AS nyckel_pavarkan, pav AS paverkan, registrerat_besluts AS registrerat_besluts " +
+            sql = "SELECT TO_CHAR(plan_id) AS nyckel, beskrivning AS beskrivning, TO_CHAR(pav_plan_id) AS nyckel_pavarkan, pav AS paverkan, registrerat_beslut AS registrerat_beslut " +
                   "FROM   gis_v_planpaverkade";
 
             DataTable dtPlanBerorPlan = new DataTable();
