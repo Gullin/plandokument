09de6afd4d0341bcde8d403f08be5f03783f87e2
Christian Gullin <krich_80@hotmail.com>
2022-08-30 20:59:51 +0200

    Lyft ut klasser till egna filer

---
+++ src/web/class/utility/UtilityDataTable.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+
+namespace Plan.Plandokument
+{
+    public static class DataTableHelpers
+    {
+
+        /// <summary>
+        /// Sorterar datatabell efter önskad kolumn och sorteringsordning. Tar endast hänsyn till en kolumn och en sorteringsordning.
+        /// </summary>
+        /// <param name="sortableTable">
+        /// Datatabell att sortera.
+        /// </param>
+        /// <param name="sortableColumn">
+        /// Kolumn i datatabell som datatabellen ska sorteras på.
+        /// </param>
+        /// <param name="sortOrder">
+        /// Sorteringsordning DESC = fallande | ASC = stigande
+        /// </param>
+        /// <returns>
+        /// Sorterad datatabell.
+        /// </returns>
+        internal static DataTable getTableSorted(DataTable sortableTable, string sortableColumn, string sortOrder)
+        {
+            DataView v = sortableTable.DefaultView;
+            v.Sort = sortableColumn + " " + sortOrder;
+            sortableTable = v.ToTable();
+            return sortableTable;
+        }
+
+        /// <summary>
+        /// Sorterar datatabell efter önskad kolumn och sorteringsordning. Tar hänsyn till två kolumner och sorteringsordning.
+        /// </summary>
+        /// <param name="sortableTable">
+        /// Datatabell att sortera.
+        /// </param>
+        /// <param name="sortableColumn1">
+        /// Kolumn i datatabell som datatabellen ska sorteras på.
+        /// </param>
+        /// <param name="sortOrder1">
+        /// Sorteringsordning DESC = fallande | ASC = stigande
+        /// </param>
+        /// <param name="sortableColumn2">
+        /// Kolumn i datatabell som datatabellen ska sorteras på.
+        /// </param>
+        /// <param name="sortOrder2">
+        /// Sorteringsordning DESC = fallande | ASC = stigande
+        /// </param>
+        /// <returns>
+        /// Sorterad datatabell.
+        /// </returns>
+        internal static DataTable getTableSorted(DataTable sortableTable, string sortableColumn1, string sortOrder1, string sortableColumn2, string sortOrder2)
+        {
+            DataView v = sortableTable.DefaultView;
+            v.Sort = sortableColumn1 + " " + sortOrder1 + ", " + sortableColumn2 + " " + sortOrder2;
+            sortableTable = v.ToTable();
+            return sortableTable;
+        }
+    }
+}
---
+++ src/web/class/utility/UtilityJSON.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Web.Script.Serialization;
+using System;
+
+namespace Plan.Plandokument
+{
+    public static class JSONHelpers
+    {
+        /// <summary>
+        /// Konverterar datatabell till lista av dictionary.
+        /// (Anledningen är avsaknad av stöd för serialisering av datatabell till JSON i .NET)
+        /// </summary>
+        /// <param name="dataTable">
+        /// Datatabell enligt .Net DataTable.
+        /// </param>
+        /// <returns>
+        /// JSON-serialiserad string.
+        /// </returns>
+        internal static string getObjectAsJson(DataTable dataTable)
+        {
+            List<Dictionary<string, Object>> rows = new List<Dictionary<string, object>>();
+            Dictionary<string, Object> row = new Dictionary<string, object>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                row = new Dictionary<string, object>();
+                foreach (DataColumn col in dataTable.Columns)
+                {
+                    row.Add(col.ColumnName, dr[col]);
+                }
+                rows.Add(row);
+            }
+
+            JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+            return jsonSerializer.Serialize(rows);
+        }
+
+        internal static string getObjectAsJson(List<Documenttype> list)
+        {
+            List<Dictionary<string, Object>> rows = new List<Dictionary<string, object>>();
+            Dictionary<string, Object> row = new Dictionary<string, object>();
+
+            foreach (var dt in list)
+            {
+                row = new Dictionary<string, object>();
+                row.Add("Type", dt.Type);
+                row.Add("UrlFilter", dt.UrlFilter);
+                row.Add("Suffix", dt.Suffix);
+                row.Add("Description", dt.Description);
+                row.Add("IsPlanhandling", dt.IsPlanhandling);
+                rows.Add(row);
+            }
+
+            JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+            return jsonSerializer.Serialize(rows);
+        }
+
+    }
+}
