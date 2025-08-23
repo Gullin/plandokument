0fae5ee08711e65ed4feb2a7dac2acfc68fa7351
Christian Gullin <krich_80@hotmail.com>
2025-08-23 22:02:46 +0200

    Konfigurationsmodellering av parametrar för kommande bakgrundskartor i ny kartklient

--- src/web/js/config.js
+++ src/web/js/config.js
@@ -22,7 +22,71 @@ extend(Lkr, {
         Setting: {
             Map: {
                 mapResizeTolerance: 0.1,    // skillnaden mellan nuvarande och ny storlek på kartbehållare (0 - 1)
-                mapResizeTimespan: 500      // i millisekunder
+                mapResizeTimespan: 500,      // i millisekunder
+                backGroundLayerSettings: {
+                    defaultMapByLayer: 'landskrona-bakgrundskarta-color-notext',
+                    backgroundtypes: [
+                        {
+                            title: 'Landskrona',
+                            credential: {
+                                token: ''
+                            },
+                            maps: [
+                                {
+                                    title: 'Landskrona Bakgrundskarta färg textfri',
+                                    url: 'https://map-services.landskrona.se/maps/v1/bakgrundskarta-color-notext',
+                                    params: {
+                                        layers: 'landskrona-bakgrundskarta-color-notext'
+                                    }
+                                },
+                                {
+                                    title: 'Landskrona Bakgrundskarta nedtonad textfri',
+                                    url: 'https://map-services.landskrona.se/maps/v1/bakgrundskarta-toned-down-notext',
+                                    params: {
+                                        layers: 'landskrona-bakgrundskarta-toned-down-notext'
+                                    }
+                                }
+                            ]
+                        },
+                        /*
+                        // Externa resurser. Bygger på att reverse proxy fungerar. Per 2025-08-14 gör en implementation i Web.config inte det.
+                        {
+                            title: 'Lantmäteriet',
+                            credential: {
+                                basic: {
+                                    user: '',
+                                    password: ''
+                                }
+                            },
+                            maps: [
+                                {
+                                    title: 'Topografiska webbkartan färg',
+                                    // url: 'https://maps.lantmateriet.se/topowebb/wms/v1?request=GetCapabilities&version=1.1.1',
+                                    // url: 'https://maps.lantmateriet.se/topowebb/wms/v1',
+                                    url: '/app/plan/proxy/lm-topowebb/topowebb/wms/v1/',
+                                    params: {
+                                        layers: 'topowebbkartan'
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            title: 'Trafikverket',
+                            credential: {
+                            },
+                            maps: [
+                                {
+                                    title: 'Nätinformation',
+                                    url: '/app/plan/proxy/trv-netinfo_1_8/MapService/wms.axd/NetInfo_1_8/',
+                                    params: {
+                                        layers: 'Vagtrafiknat'
+                                    },
+                                }
+                            ]
+                        }
+                        */
+                    ]
+                }
             }
         },
         Dokument: {
